2c6cc8ccf454d9e2627d3951209d6513161cf51e
Claudio Morais <claudiopdcgdm@gmail.com>
2021-03-18 18:12:46 -0300

    to back setting.py

--- loja/settings.py
+++ loja/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '=0)b%lv+c7p4+zd*%&rc%t_74v^zobgpd@u*-p(k#q-4$i+m!w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,12 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join('templates/static')
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join('templates/static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
